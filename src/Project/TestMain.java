edfcbe4917605f777df4b7b61f2b76749ca79966
Hasan ALAGOZ <123511537+alagozhasan@users.noreply.github.com>
2023-02-24 23:52:53 +0300

    TestMain yazıldı

--- src/Project/TestMain.java
+++ src/Project/TestMain.java
@@ -8,27 +8,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class TestMain extends BaseDriver {
-    //    Url: https://itera-qa.azurewebsites.net/
-    //
-    // *[text()='Sign Up']/../a /signup
+
     @Test
     public void Test1() {
+        String yenihesap="tsgroup009";//bu test yeni hesap olduğu için burda username atamamız gerek
         driver.get("https://itera-qa.azurewebsites.net/");
         WebElement signupbtn = driver.findElement(By.xpath("//*[text()='Sign Up']/../a"));
         signupbtn.click();
 
-        WebElement singUp=driver.findElement(By.linkText("Sing Up"));
+//        WebElement singUp = driver.findElement(By.linkText("Sing Up"));
 
         Myfunc.Bekle(1);
         //Form
-        WebElement firstname= driver.findElement(By.cssSelector("[class='form-group']>div>input[id='FirstName']"));
-        WebElement surname= driver.findElement(By.cssSelector("[class='form-group']>div>input[id='Surname']"));
-        WebElement eposta= driver.findElement(By.cssSelector("[class='form-group']>div>input[id='E_post']"));
-        WebElement mobile= driver.findElement(By.cssSelector("[class='form-group']>div>input[id='Mobile']"));
-        WebElement username= driver.findElement(By.cssSelector("[class='form-group']>div>input[id='Username']"));
-        WebElement password= driver.findElement(By.cssSelector("[class='form-group']>div>input[id='Password']"));
-        WebElement confirmpassword= driver.findElement(By.cssSelector("[class='form-group']>div>input[id='ConfirmPassword']"));
-        WebElement submitbtn= driver.findElement(By.cssSelector("[id='submit']"));
+        WebElement firstname = driver.findElement(By.cssSelector("[class='form-group']>div>input[id='FirstName']"));
+        WebElement surname = driver.findElement(By.cssSelector("[class='form-group']>div>input[id='Surname']"));
+        WebElement eposta = driver.findElement(By.cssSelector("[class='form-group']>div>input[id='E_post']"));
+        WebElement mobile = driver.findElement(By.cssSelector("[class='form-group']>div>input[id='Mobile']"));
+        WebElement username = driver.findElement(By.cssSelector("[class='form-group']>div>input[id='Username']"));
+        WebElement password = driver.findElement(By.cssSelector("[class='form-group']>div>input[id='Password']"));
+        WebElement confirmpassword = driver.findElement(By.cssSelector("[class='form-group']>div>input[id='ConfirmPassword']"));
+        WebElement submitbtn = driver.findElement(By.cssSelector("[id='submit']"));
         //*xpath
 //        WebElement firstname = driver.findElement(By.xpath("(//*[@class='col-md-10']/input)[1]"));
 //        WebElement surname = driver.findElement(By.xpath("(//*[@class='col-md-10']/input)[2]"));
@@ -40,10 +39,10 @@ public class TestMain extends BaseDriver {
 //        WebElement submitbtn = driver.findElement(By.xpath("//*[@id='submit']"));
         //*formdoldur
         firstname.click();
-        firstname.sendKeys("Hasaan");
+        firstname.sendKeys("Java");
         Myfunc.Bekle(1);
         surname.click();
-        surname.sendKeys("Alaagoz");
+        surname.sendKeys("Grup9");
         Myfunc.Bekle(1);
         eposta.click();
         eposta.sendKeys("0sahipkiranemirtimur0@gmail.com");
@@ -53,30 +52,127 @@ public class TestMain extends BaseDriver {
         Myfunc.Bekle(1);
 
         username.click();
-        username.sendKeys("MarcusAureliusa");
+        username.sendKeys(yenihesap);//burası için
         Myfunc.Bekle(1);
 
         password.click();
-        password.sendKeys("12345armin");
+        password.sendKeys(getPassword());
         Myfunc.Bekle(1);
 
         confirmpassword.click();
-        confirmpassword.sendKeys("12345armin");
+        confirmpassword.sendKeys(getPassword());
         Myfunc.Bekle(1);
         submitbtn.click();
         Myfunc.Bekle(2);
 
-        WebElement labelsucces=driver.findElement(By.cssSelector("[class='label-success']"));
+        WebElement labelsucces = driver.findElement(By.cssSelector("[class='label-success']"));
 //        WebElement labelsucces=driver.findElement(By.xpath("//label[@class='label-success']"));
-        String label=labelsucces.getText();
-        Assert.assertTrue("TessetPassed",label.equals("Registration Successful"));
+        String label = labelsucces.getText();
+        Assert.assertTrue("TestPassed",  label.equals("Registration Successful"));
 
 
-//[class='label-success']
-//
-
-
-        BekleKapat();
+//        BekleKapat();
     }
 
+    @Test
+    public void Test2() {
+//        driver.get("https://itera-qa.azurewebsites.net/");
+////        WebElement Login = driver.findElement(By.linkText("Login"));
+//        WebElement Login = driver.findElement(By.xpath("//a[text()='Login']/../a"));
+//        Login.click();
+//       WebElement username = driver.findElement(By.cssSelector("input[id='Username']"));
+//        WebElement password = driver.findElement(By.cssSelector("input[id='Password']"));
+////WebElement username=driver.findElement(By.xpath("//input[@id='Username']"));
+////WebElement password=driver.findElement(By.xpath("//input[@id='Password']"));
+//        WebElement loginbtn = driver.findElement(By.xpath("//input[@name='login']"));
+//        Myfunc.Bekle(3);
+//        username.click();
+//        username.sendKeys(getKullaniciadi());
+//        Myfunc.Bekle(1);
+//        password.click();
+//        password.sendKeys(getPassword());
+//        Myfunc.Bekle(1);
+//        loginbtn.click();
+//        Myfunc.Bekle(3);
+
+
+        Login();
+        Myfunc.Bekle(3);
+        String currentUrl = driver.getCurrentUrl();//o anki web sitesini String olaran döndüren method
+//        WebElement dashbord = driver.findElement(By.xpath("div>h1"));
+//        String text = dashbord.getText();
+
+        Assert.assertTrue("Test Passed", currentUrl.equals("https://itera-qa.azurewebsites.net/Dashboard"));
+//        WebElement logout = driver.findElement(By.xpath("//a[text()='Log out']"));
+//        logout.click();
+        Logout();
+        BekleKapat();
+
+    }
+
+
+    @Test
+    public void Test3() {
+
+//        driver.get("https://itera-qa.azurewebsites.net/");
+//        WebElement Login = driver.findElement(By.xpath("//a[text()='Login']/../a"));
+//        Login.click();
+////        Login();
+//        WebElement username = driver.findElement(By.xpath("//input[@id='Username']"));
+//        WebElement password = driver.findElement(By.xpath("//input[@id='Password']"));
+//        WebElement loginbtn = driver.findElement(By.xpath("//input[@name='login']"));
+//        Myfunc.Bekle(3);
+//        username.click();
+//        username.sendKeys(getKullaniciadi());
+//        Myfunc.Bekle(1);
+//        password.click();
+//            password.sendKeys(getPassword());
+//        Myfunc.Bekle(1);
+//        loginbtn.click();
+//        Myfunc.Bekle(3);
+Login();
+
+        WebElement creatbtn = driver.findElement(By.cssSelector("div[class='container body-content'] a[class='btn btn-primary']"));
+//        WebElement creatbtn=driver.findElement(By.xpath("//div[@class='container body-content']//a[@class='btn btn-primary']"));
+        creatbtn.click();
+        Myfunc.Bekle(2);
+        WebElement name = driver.findElement(By.xpath("(//div[@class='col-md-10']/input)[1]"));
+        WebElement company = driver.findElement(By.xpath("(//div[@class='col-md-10']/input)[2]"));
+        WebElement address = driver.findElement(By.xpath("(//div[@class='col-md-10']/input)[3]"));
+        WebElement city = driver.findElement(By.xpath("(//div[@class='col-md-10']/input)[4]"));
+        WebElement phone = driver.findElement(By.xpath("(//div[@class='col-md-10']/input)[5]"));
+        WebElement email = driver.findElement(By.xpath("(//div[@class='col-md-10']/input)[6]"));
+        WebElement submitbtn = driver.findElement(By.xpath("//input[@type='submit']"));
+        name.click();
+        name.sendKeys("Cimbom");
+        Myfunc.Bekle(1);
+        company.click();
+        company.sendKeys("Galatasaray");
+        Myfunc.Bekle(1);
+        address.click();
+        address.sendKeys("Florya");
+        Myfunc.Bekle(1);
+        city.click();
+        city.sendKeys("Istanbul");
+        Myfunc.Bekle(1);
+        phone.click();
+        phone.sendKeys("1905");
+        Myfunc.Bekle(1);
+        email.click();
+        email.sendKeys("hasanftceza_crazyboy@hotmail.com");
+        submitbtn.click();
+        String currenturl = driver.getCurrentUrl();
+
+        Assert.assertTrue("TestPassed", currenturl.equals("https://itera-qa.azurewebsites.net/Dashboard"));
+
+//        WebElement logout = driver.findElement(By.xpath("//a[text()='Log out']"));
+//        logout.click();
+        Logout();
+//        BekleKapat();
+    }
 }
+
+
+
+
+
--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -1,6 +1,8 @@
 package Utility;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 
@@ -10,6 +12,30 @@ import java.util.logging.Logger;
 
 public class BaseDriver {
     public static WebDriver driver;
+    public static String kullaniciadi="therationalmale0";
+    public static String password="123rollo";
+
+    public BaseDriver() {
+    }
+    public BaseDriver(String username,String password) {
+        setKullaniciadi(username);
+        setPassword(password);
+    }
+    public static String getPassword() {
+        return password;
+    }
+
+    public static void setPassword(String password) {
+        BaseDriver.password = password;
+    }
+
+    public static String getKullaniciadi() {
+        return kullaniciadi;
+    }
+
+    public static void setKullaniciadi(String kullaniciadi) {
+        BaseDriver.kullaniciadi = kullaniciadi;
+    }
 
     static {
         KalanOncekileriKapat();
@@ -20,12 +46,15 @@ public class BaseDriver {
         Duration dr=Duration.ofSeconds(30);//Web sitesnide elementlerin yüklenmesi ile alakalı
         driver.manage().timeouts().pageLoadTimeout(dr);
         driver.manage().timeouts().implicitlyWait(dr);//
+
+
+
     }
 
 
     public static void BekleKapat() {
         Myfunc.Bekle(3);
-        driver.quit();
+        driver.close();
     }
 
 
@@ -36,5 +65,27 @@ public class BaseDriver {
         } catch (Exception ignored) {
 
         }
+    }
+
+    public static void Login(){
+        driver.get("https://itera-qa.azurewebsites.net/");
+        WebElement Login = driver.findElement(By.xpath("//a[text()='Login']/../a"));
+        Login.click();
+        WebElement username = driver.findElement(By.cssSelector("input[id='Username']"));
+        WebElement password = driver.findElement(By.cssSelector("input[id='Password']"));
+        WebElement loginbtn = driver.findElement(By.xpath("//input[@name='login']"));
+        username.click();
+        username.sendKeys(getKullaniciadi());
+        Myfunc.Bekle(1);
+        password.click();
+        password.sendKeys(getPassword());
+        Myfunc.Bekle(1);
+        loginbtn.click();
+        Myfunc.Bekle(3);
+
+    }
+    public static void Logout(){
+        WebElement logout = driver.findElement(By.xpath("//a[text()='Log out']"));
+        logout.click();
     }
 }
